e23465a2238000939fd27b9b752f2cfca39f5b3e
Daniel Smit <dcsmit7@gmail.com>
2021-12-03 23:56:15 +1100

    Address failing unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+declare global {
+  namespace NodeJS {
+    interface Global {
+       document: Document;
+       window: Window;
+       navigator: Navigator;
+    } 
+  }
+}
+
+jest.mock('use-resize-observer', () => () => ({
+  __esModule: true,
+  default: jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn(),
+  })),
+}));
+
+test('renders Strategy Game Site', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/Strategy Game Site/i);
   expect(linkElement).toBeInTheDocument();
 });
