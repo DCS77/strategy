802f8397c43d0c2b8d95a0fa4ce3cf306b117803
Daniel Smit <dcsmit7@gmail.com>
2022-01-06 21:54:21 +1030

    Check some links direct to correct pages

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,31 +1,52 @@
 /* eslint-disable no-undef */
+import React from 'react';
 import '@testing-library/jest-dom';
 import { mount, shallow } from 'enzyme';
-import { render } from '@testing-library/react';
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 import Main from '../pages/main';
 
-it('renders without crashing', () => {
-  shallow(<App />);
-});
+describe('App', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
 
-it('renders main component', () => {
-  const wrapper = mount(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
-  expect(wrapper.contains(<Main />)).toEqual(true);
-});
+  it('renders main component', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(wrapper.contains(<Main />)).toEqual(true);
+  });
 
-it('renders ReadyCompete text', () => {
-  const layout = render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
-  const titleElement = layout.getByText(/ReadyCompete/i);
-  expect(titleElement).toBeInTheDocument();
+  it('ReadyCompete links to home page', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const link = screen.getByRole('link', { name: /ReadyCompete/i });
+    userEvent.click(link);
+    await waitFor(() => expect(document.title).toMatch(/RC | ReadyCompete Home/i));
+  });
+
+  it('TV links to correct page', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const link = screen.getAllByRole('link', { name: /Arena TV/i });
+
+    userEvent.click(link[0]);
+    expect(document.title).toMatch(/RC | Arena TV/i);
+
+    userEvent.click(link[1]);
+    expect(document.title).toMatch(/RC | Arena TV/i);
+  });
 });
