d943c722b90e212ae53a07f2909571c4c470f52b
Daniel Smit <dcsmit7@gmail.com>
2022-01-07 23:23:02 +1030

    Lazy load page content and show loading animation

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,17 +1,29 @@
-import React, { useRef, useState } from 'react';
+import React, {
+  lazy, Suspense, useRef, useState,
+} from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
+import { PacmanLoader } from 'react-spinners';
 import { useStateValue } from '../state/state';
-import Home from './home';
-import AI from './ai';
-import Army from './army/army';
-import CreateArmy from './army/createArmy';
-import Leaderboards from './leaderboards';
-import Learn from './learn';
-import Play from './play';
-import TV from './tv';
 import BottomBar from '../components/bottomBar/bottomBar';
 import TopBarGroup from '../components/topBar/topBarGroup';
 import GameTabBar from '../components/tabBar/gameTabBar';
+
+const Home = lazy(() => import('./home'));
+const AI = lazy(() => import('./ai'));
+const Army = lazy(() => import('./army/army'));
+const CreateArmy = lazy(() => import('./army/createArmy'));
+const Leaderboards = lazy(() => import('./leaderboards'));
+const Learn = lazy(() => import('./learn'));
+const Play = lazy(() => import('./play'));
+const TV = lazy(() => import('./tv'));
+
+const LoadingPage = () => (
+  <div className='full-size'>
+    <div className='absolute-centre'>
+      <PacmanLoader color='orange' />
+    </div>
+  </div>
+);
 
 const GameTab = () => {
   const splitPath = useLocation().pathname.split('/');
@@ -54,23 +66,25 @@ const Main = () => {
       <TopBarGroup />
       <div id='page' className='page'>
         <GameTabBar onHeightChange={onTabBarHeightChange} />
-        <Switch>
-          <Route path='/play'><GameTab /></Route>
-          <Route path='/info'>Info 1</Route>
-          <Route path='/tv'><TVTab /></Route>
-          <Route path='/ai'><AITab /></Route>
-          <Route path='/learn'><LearnTab /></Route>
-          <Route path='/army/:id?' component={Army} />
-          <Route path='/create'><CreateArmyTab /></Route>
-          <Route path='/leaderboards'><LeaderboardsTab /></Route>
-          <Route path='/online'>Online</Route>
-          <Route path='/live'>Active games</Route>
-          <Route path='/contact'>Contact</Route>
-          <Route path='/donate'>Donate</Route>
-          <Route path='/terms'>Terms</Route>
-          <Route path='/privacy'>Privacy</Route>
-          <Route path='/'><Home /></Route>
-        </Switch>
+        <Suspense fallback={<LoadingPage />}>
+          <Switch>
+            <Route path='/play'><GameTab /></Route>
+            <Route path='/info'>Info 1</Route>
+            <Route path='/tv'><TVTab /></Route>
+            <Route path='/ai'><AITab /></Route>
+            <Route path='/learn'><LearnTab /></Route>
+            <Route path='/army/:id?' component={Army} />
+            <Route path='/create'><CreateArmyTab /></Route>
+            <Route path='/leaderboards'><LeaderboardsTab /></Route>
+            <Route path='/online'>Online</Route>
+            <Route path='/live'>Active games</Route>
+            <Route path='/contact'>Contact</Route>
+            <Route path='/donate'>Donate</Route>
+            <Route path='/terms'>Terms</Route>
+            <Route path='/privacy'>Privacy</Route>
+            <Route path='/'><Home /></Route>
+          </Switch>
+        </Suspense>
       </div>
       <BottomBar />
     </div>
